80883d33e6dccada18c28359077e9e30ba3c868e
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-04-04 16:27:36 -0300

    add props to expenseItem

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,7 +1,55 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "car insurance",
+      amount: 294.78,
+      date: new Date(),
+    },
+    {
+      id: "e2",
+      title: "car oil",
+      amount: 120.99,
+      date: new Date(),
+    },
+    {
+      id: "e3",
+      title: "car wash",
+      amount: 94.78,
+      date: new Date(),
+    },
+    {
+      id: "e4",
+      title: "car ",
+      amount: 1094.78,
+      date: new Date(),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
