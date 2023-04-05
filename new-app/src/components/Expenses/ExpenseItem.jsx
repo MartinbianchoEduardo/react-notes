a0dc26605043b63d45fae0aa47fb112412fd02bc
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-04-05 12:01:37 -0300

    setup button state

---
+++ new-app/src/components/Expenses/ExpenseItem.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+// the useState function allows to define values as state
+// any change in these value will make the component function be called again
+// remember all components are functions
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+function ExpenseItem(props) {
+  //useState( default state value )
+  //returns an array
+  //the first element is the current state value itself (the 'variable')
+  //the second element is the update function
+  //using destructuring
+  const [title, setTitle] = useState(props.title);
+  //title is a pointer to props.title
+
+  const clickHandler = () => {
+    //passing the new value as parameter
+    setTitle("new title");
+    //when calling setTitle (the update function)
+    //we tell react that a new value is being assigned to this state
+    //and this will make react call the component function again
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
