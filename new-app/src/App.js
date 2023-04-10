59f3b4ac81603d1296291998528c2bae2ea526bf
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-04-10 13:58:30 -0300

    add filter render

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -5,27 +5,27 @@ import NewExpense from "./components/NewExpense/NewExpense";
 const initialExpenses = [
   {
     id: "e1",
-    title: "car insurance",
-    amount: 294.78,
-    date: new Date(),
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
   },
   {
     id: "e2",
-    title: "car oil",
-    amount: 120.99,
-    date: new Date(),
+    title: "New LED TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
   },
   {
     id: "e3",
-    title: "car wash",
-    amount: 94.78,
-    date: new Date(),
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
   },
   {
     id: "e4",
-    title: "car ",
-    amount: 1094.78,
-    date: new Date(),
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
   },
 ];
 
--- new-app/src/components/Expenses/Expenses.js
+++ new-app/src/components/Expenses/Expenses.js
@@ -5,7 +5,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2022");
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -19,13 +19,21 @@ function Expenses(props) {
           onChangeFilter={filterChangeHandler}
         />
 
-        {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {props.items
+          .filter(
+            (expense) => expense.date.getFullYear().toString() === filteredYear
+          )
+          .map((expense) => {
+            return (
+              <ExpenseItem
+                key={expense.id} //key is used so react identifies each element in the list
+                //so it doesn't change all items when adding a new one to match the previous order
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+              />
+            );
+          })}
       </Card>
     </div>
   );
