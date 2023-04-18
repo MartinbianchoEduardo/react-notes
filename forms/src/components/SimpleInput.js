0dcb84df89489c0e9fe65769cf7b0d2f863abce7
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-04-18 10:05:05 -0300

    add basic input validation

--- forms/src/components/SimpleInput.js
+++ forms/src/components/SimpleInput.js
@@ -1,10 +1,38 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+    console.log(enteredName);
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          value={enteredName}
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
+      {!enteredNameIsValid && <p className="error-text">invalid name</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
