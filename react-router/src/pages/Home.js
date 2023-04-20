9cdf493ed7412071159aa7c39ce7b8181bf49123
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-04-20 12:10:26 -0300

    add useNavigate example

--- react-router/src/pages/Home.js
+++ react-router/src/pages/Home.js
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate(); //switch to a different route from inside code
+
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
   return (
     <div>
       <h1>My home page</h1>
       <p>
         Go to <Link to="/products">products</Link>
       </p>
+      <button onClick={navigateHandler}>navigate using useNavigate</button>
     </div>
   );
 };
