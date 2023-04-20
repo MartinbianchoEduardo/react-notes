c90c3f9295232dad1f116a8fbb44a9b80ee58d23
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-04-20 15:25:47 -0300

    add events to eventsPage

--- react-router-project/frontend/src/pages/EventsPage.js
+++ react-router-project/frontend/src/pages/EventsPage.js
@@ -1,5 +1,44 @@
+import EventItem from "../components/EventItem";
+
 const EventsPage = () => {
-  return <h1>Events Page</h1>;
+  const EVENTS = [
+    {
+      id: "e1",
+      title: "A dummy event",
+      date: "2023-02-22",
+      image:
+        "https://blog.hubspot.de/hubfs/Germany/Blog_images/Optimize_Marketing%20Events%20DACH%202021.jpg",
+      description:
+        "Join this amazing event and connect with fellow developers.",
+    },
+  ];
+
+  return (
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        {EVENTS.map((e) => (
+          <EventItem event={e} />
+        ))}
+      </ul>
+    </>
+  );
 };
+
+/* <>
+      <h1>Events Page</h1>
+      <ul>
+        {EVENTS.map((e) => {
+          return (
+            <>
+              <h2>{e.title}</h2>
+              <p>Date: {e.date}</p>
+              <img src={e.image} alt="event" />
+              <p>{e.description}</p>
+            </>
+          );
+        })}
+      </ul>
+    </> */
 
 export default EventsPage;
