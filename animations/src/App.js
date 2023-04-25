aa7a68788d436c0825fd9f907465b46aa07ecbdc
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-04-25 11:04:17 -0300

    add animation package

--- animations/src/App.js
+++ animations/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, useState } from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -8,6 +9,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -22,6 +24,40 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+
+        <Transition
+          in={this.state.showBlock}
+          timeout={500}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "orangered",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                marginTop: 15,
+                marginBottom: 15,
+                transition: "opacity 1s ease",
+                opacity: state === "exited" ? 0 : 1,
+              }}
+            >
+              {state}
+            </div>
+          )}
+        </Transition>
+
         <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         <Backdrop show={this.state.modalIsOpen} />
         <button className="Button" onClick={this.showModal}>
