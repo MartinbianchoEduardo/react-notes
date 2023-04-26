b6e9661d867a39c66c372667ef8eb71b8d61e908
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-04-26 11:12:44 -0300

    describe Greeting test

--- tests/src/components/Greeting.test.js
+++ tests/src/components/Greeting.test.js
@@ -1,17 +1,20 @@
 import Greeting from "./Greeting";
 import { render, screen } from "@testing-library/react";
 
-test("renders hello as text", () => {
-  //Arrange
-  //(setup test data, test conditions and environment)
-  render(<Greeting />);
+//describe the test to group all related tests
+describe("Greeting component", () => {
+  test("renders hello as text", () => {
+    //Arrange
+    //(setup test data, test conditions and environment)
+    render(<Greeting />);
 
-  //Act
-  //(run logic that should be tested)
-  // nothing in this case
+    //Act
+    //(run logic that should be tested)
+    // nothing in this case
 
-  //Assert
-  //compare result with expectations
-  const helloElement = screen.getByText("Hello");
-  expect(helloElement).toBeInTheDocument();
+    //Assert
+    //compare result with expectations
+    const helloElement = screen.getByText("Hello");
+    expect(helloElement).toBeInTheDocument();
+  });
 });
