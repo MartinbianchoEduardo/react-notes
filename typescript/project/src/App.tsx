a057e84c08b4726da68459b43a33af3112252f56
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2023-04-28 11:57:21 -0300

    add add todos

--- typescript/project/src/App.tsx
+++ typescript/project/src/App.tsx
@@ -1,13 +1,24 @@
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
-
-const todos = [new Todo("Learn React"), new Todo("Learn JavaScript")];
+import { useState } from "react";
 
 function App() {
+  //specify the type of elements of the array <Todo[]> and the starting value (empty array) ([])
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+      //.concat in order to return a new array
+    });
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
